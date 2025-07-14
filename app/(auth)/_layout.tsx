edd9933b07d7a8a23bbc080db7fc6551ec7a3978
RedRedRyan <ryankipkorir2002@gmail.com>
2025-07-14 15:13:34 +0300

    inputmail

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -2,6 +2,8 @@ import {View, Text, KeyboardAvoidingView, ScrollView, Dimensions, ImageBackgroun
 import React from 'react'
 import {Slot} from "expo-router";
 import {images} from "@/constants";
+import CustomButton from "@/components/CustomButton";
+import CustomInput from "@/components/CustomInput";
 
 export default function _Layout() {
     return (
@@ -11,6 +13,13 @@ export default function _Layout() {
                     <ImageBackground source={images.loginGraphic} className={'size-full rounded-b-lg'} resizeMode='stretch' />
                     <Image source={images.logo} className={'self-center size-48 absolute -bottom-16 z-10'}/>
                 </View>
+                <CustomInput
+                placeholder={'Enter your email'}
+                value={''}
+                onChangeText={(text)=>{}}
+                label={'Email'}
+                keyboardType='email-address'/>
+                <CustomButton/>
             </ScrollView>
             <Slot />
         </KeyboardAvoidingView>
--- components/CustomInput.tsx
+++ components/CustomInput.tsx
@@ -1,10 +1,34 @@
-import {View, Text} from 'react-native'
-import React from 'react'
+import {View, Text, TextInput} from 'react-native'
+import {CustomInputProps} from "@/type";
+import cn from 'clsx';
+import {useState} from "react";
 
-const CustomInput = () => {
+const CustomInput = ({
+    placeholder='Enter text',
+    value,
+    onChangeText,
+    label,
+    secureTextEntery= false,
+    keyboardType='default',
+                     }: CustomInputProps) => {
+    const [isFocused, setIsFocused] = useState(false);
+
     return (
-        <View>
-            <Text>CustomInput</Text>
+        <View className={'w-full'}>
+            <Text className={'label'}>{label}</Text>
+            <TextInput
+            autoCapitalize={'none'}
+            autoCorrect={false}
+            value={value}
+            onChangeText={onChangeText}
+            secureTextEntry={secureTextEntery}
+            keyboardType={keyboardType}
+            onFocus={()=>setIsFocused(true)}
+            onBlur={()=>setIsFocused(false)}
+            placeholder={placeholder}
+            placeholderTextColor={'#888 '}
+            className={cn('input', isFocused ? 'border-primary': 'border-gray-300')}
+            />
         </View>
     )
 }
