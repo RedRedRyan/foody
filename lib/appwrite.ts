6a533a9a4aaea627694d0e5ecfc174fb8c0947a4
RedRedRyan <ryankipkorir2002@gmail.com>
2025-07-14 14:46:23 +0300

    sign-in

---
+++ lib/appwrite.ts
@@ -0,0 +1,8 @@
+export const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    platform: 'com.jsm.foody',
+    databaseId: '6874e7310032b2bc3692',
+    userCollectionId: '6874e7a70022a90bb228',
+
+}
