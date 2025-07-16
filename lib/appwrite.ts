5e3febe3f7c80d2de68523255fd3761f3e9cad0b
RedRedRyan <ryankipkorir2002@gmail.com>
2025-07-16 13:28:28 +0300

    appwrite sign in

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,3 +1,7 @@
+import {ID, Account, Avatars, Client, Databases, Query} from "react-native-appwrite";
+import {CreateUserParams, SignInParams} from "@/type";
+
+
 export const appwriteConfig = {
     endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
     projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
@@ -5,4 +9,65 @@ export const appwriteConfig = {
     databaseId: '6874e7310032b2bc3692',
     userCollectionId: '6874e7a70022a90bb228',
 
+}
+
+export const client= new Client();
+
+client
+        .setEndpoint(appwriteConfig.endpoint)
+        .setProject(appwriteConfig.projectId)
+        .setPlatform(appwriteConfig.platform)
+export const account = new Account(client);
+export const databases = new Databases(client);
+const avatars = new Avatars(client);
+
+export const createUser = async ({email, password, name}: CreateUserParams) => {
+    try {
+        const newAccount = await account.create(ID.unique(), email, password, name);
+        if(!newAccount) throw  Error;
+
+        await signIn({email, password});
+        const avatarUrl = avatars.getInitials(name);
+
+        return  await databases.createDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.userCollectionId,
+            ID.unique(),
+            {accountId: newAccount.$id,email, name, avatar: avatarUrl}
+        );
+
+    } catch (e) {
+        throw new Error(e as string);
+
+    }
+}
+
+export const signIn = async ({email, password}: SignInParams)=>{
+    try {
+        const session = await account.createEmailPasswordSession(email,password);
+
+    }catch (e) {
+        throw new  Error(e as string);
+
+    }
+
+}
+export const getCurrentUser = async ()=>{
+    try {
+        const currentAccount = await account.get();
+        if(!currentAccount) throw  Error;
+
+        const currentUser = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.userCollectionId,
+            [Query.equal('accountId', currentAccount.$id)],
+        )
+        if(!currentUser) throw  Error;
+        return currentUser.documents[0];
+
+    }catch (e) {
+        console.log(e);
+        throw new  Error(e as string);
+
+    }
 }
