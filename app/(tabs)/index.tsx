c44bbff0b966c5a0808e759b12331c07f5c5e6c9
RedRedRyan <ryankipkorir2002@gmail.com>
2025-07-20 22:43:21 +0300

    Tab Nav

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -10,7 +10,7 @@ import useAuthStore from "@/store/auth.store";
 
 export default function Index() {
     const {user} =useAuthStore();
-    console.log("UseAuthStore",user);
+
   return (
     <SafeAreaView className={'flex-1 bg-white'}>
 
